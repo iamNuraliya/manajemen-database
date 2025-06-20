c6332dff2f1a211c96a4f419e5fa68eb8bfada03
iamNuraliya <155157567+iamNuraliya@users.noreply.github.com>
2025-06-20 10:34:09 +0700

    Update perpustakaan.js

--- perpustakaan.js
+++ perpustakaan.js
@@ -1,7 +1,1 @@
-use Perpustakaan
 
-db.buku.insertMany([
-  { judul: "Pemrograman Java", penulis: "Andi", tahun: 2021 },
-  { judul: "Dasar MongoDB", penulis: "Budi", tahun: 2022 },
-  { judul: "Struktur Data", penulis: "Citra", tahun: 2020 }
-])
